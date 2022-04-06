1fd84f7732617690df2f90a821f12ea610a307c1
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2022-04-06 15:26:15 +0200

    Warn when kind cluster already exists

--- src/kind.rs
+++ src/kind.rs
@@ -1,3 +1,5 @@
+use crate::helpers;
+use log::warn;
 use std::io::Write;
 use std::process::{Command, Stdio};
 
@@ -29,20 +31,29 @@ nodes:
           node-labels: node=3
 "#;
 
-pub fn create_cluster(cluster_name: &str) {
-    let child = Command::new("kind")
-        .args(["create", "cluster", "--name", cluster_name, "--config", "-"])
-        .stdin(Stdio::piped())
-        .spawn()
-        .expect("Failed to spawn kind command");
+pub fn create_cluster(name: &str) {
+    if check_if_kind_cluster_exists(name) {
+        warn!("The kind cluster {name} is already running, not creating it again");
+    } else {
+        let child = Command::new("kind")
+            .args(["create", "cluster", "--name", name, "--config", "-"])
+            .stdin(Stdio::piped())
+            .spawn()
+            .expect("Failed to spawn kind command");
 
-    child
-        .stdin
-        .as_ref()
-        .unwrap()
-        .write_all(KIND_CLUSTER_DEFINITION.as_bytes())
-        .expect("Failed to write kind cluster definition via stdin");
-    if !child.wait_with_output().unwrap().status.success() {
-        panic!("Failed to create kind cluster, see kind logs");
+        child
+            .stdin
+            .as_ref()
+            .unwrap()
+            .write_all(KIND_CLUSTER_DEFINITION.as_bytes())
+            .expect("Failed to write kind cluster definition via stdin");
+        if !child.wait_with_output().unwrap().status.success() {
+            panic!("Failed to create kind cluster, see kind logs");
+        }
     }
+}
+
+fn check_if_kind_cluster_exists(name: &str) -> bool {
+    let result = helpers::execute_command(vec!["kind", "get", "clusters"]);
+    result.lines().any(|cluster_name| cluster_name == name)
 }
