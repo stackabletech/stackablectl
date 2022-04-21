8f19694e4bfa84c9d30867ab8649da8b3ebafdf4
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2022-04-21 10:59:56 +0200

    Add cli flags to overwrite helm repo URLs

--- src/arguments.rs
+++ src/arguments.rs
@@ -11,6 +11,21 @@ pub struct CliArgs {
     /// Log level. One of Error, Warn, Info, Debug or Trace
     #[clap(short, long, default_value = "Info")]
     pub log_level: LevelFilter,
+
+    /// f you don't have access to the Stackable Helm repos you can mirror the repo and provide the URL here
+    /// (e.g. https://my.repo/repository/stackable-stable/).
+    #[clap(long, default_value = "https://repo.stackable.tech/repository/helm-stable")]
+    pub helm_repo_stackable_stable: String,
+
+    /// f you don't have access to the Stackable Helm repos you can mirror the repo and provide the URL here
+    /// (e.g. https://my.repo/repository/stackable-test/).
+    #[clap(long, default_value = "https://repo.stackable.tech/repository/helm-test")]
+    pub helm_repo_stackable_test: String,
+
+    /// f you don't have access to the Stackable Helm repos you can mirror the repo and provide the URL here
+    /// (e.g. https://my.repo/repository/stackable-dev/).
+    #[clap(long, default_value = "https://repo.stackable.tech/repository/helm-dev")]
+    pub helm_repo_stackable_dev: String,
 }
 
 #[derive(Parser)]
