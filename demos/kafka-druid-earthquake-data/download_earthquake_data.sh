38d7ca48bade19ff63e58200aba12b6f1aabb35c
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2022-08-15 15:50:26 +0200

    Update download_earthquake_data.sh

--- demos/kafka-druid-earthquake-data/download_earthquake_data.sh
+++ demos/kafka-druid-earthquake-data/download_earthquake_data.sh
@@ -1,21 +1,31 @@
 # This scripts is not used for the demo
 # Its purpose is to document how to retrive the used earthquake data
 
-# rm -f 20* earthquakes.csv
+# rm -f 19*.csv 20*.csv earthquakes.csv
 
-for year in {2000..2022}; do
+# We have to request small chunks otherwise the API will not return us any data
+for year in {1950..1999}; do
     for month in {01..12}; do
-        start="${year}-${month}-01 00:00:00"
-        end="${year}-${month}-31 23:59:59"
-        echo "Downloading $year/$month"
-        if ! test -f "${year}_${month}.csv"; then
-            curl -o "${year}_${month}.csv" "https://earthquake.usgs.gov/fdsnws/event/1/query.csv?starttime=${start}&endtime=${end}&minmagnitude=1&orderby=time-asc"
-        fi
-        if test -f earthquakes.csv; then
-            tail -n +2 "${year}_${month}.csv" >> earthquakes.csv
-        else
-            cp "${year}_${month}.csv" earthquakes.csv # Copy to preserve header
-        fi
+        # Either day 01 - 15
+        # or day     16 - 31
+        for startDay in 01 16; do
+            if [ "$startDay" == "01" ]; then
+                endDay="15"
+            else
+                endDay="31"
+            fi
+            start="${year}-${month}-${startDay} 00:00:00"
+            end="${year}-${month}-${endDay} 23:59:59"
+            echo "Downloading $year/$month/${startDay} (from $start to $end)"
+            if ! test -f "${year}_${month}_${startDay}.csv"; then
+                curl -o "${year}_${month}_${startDay}.csv" "https://earthquake.usgs.gov/fdsnws/event/1/query.csv?starttime=${start}&endtime=${end}&minmagnitude=1&orderby=time-asc"
+            fi
+            if test -f earthquakes.csv; then
+                tail -n +2 "${year}_${month}_${startDay}.csv" >> earthquakes.csv
+            else
+                cp "${year}_${month}_${startDay}.csv" earthquakes.csv # Copy to preserve header
+            fi
+        done
     done
 done
 
