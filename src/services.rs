3846cf4a5759d9fcdb5f9015025fe10540b2705c
Siegfried Weber <mail@siegfriedweber.net>
2022-12-07 13:23:51 +0100

    Configure OpenSearch users in the logging stack and show extra infos for OpenSearch Dashboards provided by the service

--- src/services.rs
+++ src/services.rs
@@ -573,64 +573,64 @@ async fn get_opensearch_dashboards_services(
 
     let mut result = Vec::new();
 
-    let deployment_api: Api<Deployment> = if namespaced {
-        Api::namespaced(client.clone(), NAMESPACE.lock()?.as_str())
+    let service_api: Api<Service> = if namespaced {
+        Api::namespaced(client.to_owned(), NAMESPACE.lock()?.as_str())
     } else {
         Api::all(client.clone())
     };
 
-    let deployments = deployment_api.list(&list_params).await?;
-    for deployment in deployments {
-        let installed_product = get_opensearch_dashboards_service(
-            &deployment.name_unchecked(),
-            &deployment
-                .namespace()
-                .ok_or("OpenSearch Dashboards deployment has no namespace")?,
-            client.clone(),
-            redact_credentials,
-        )
-        .await?;
+    let services = service_api.list(&list_params).await?;
+    for service in services {
+        let namespace = service.namespace().unwrap();
+        let endpoints =
+            get_service_endpoint_urls(&service, &service.name_unchecked(), client.to_owned())
+                .await?;
+
+        let annotations = service.annotations();
+
+        let mut extra_infos = vec!["Third party service".to_string()];
+
+        if let Some(http_endpoint) = endpoints.get("http") {
+            if let Some(logs_endpoint) = annotations.get("stackable.tech/logging-view-logs") {
+                extra_infos.push(format!("Logs view: {http_endpoint}{logs_endpoint}"));
+            }
+        }
+
+        if let Some(credentials_secret) =
+            annotations.get("stackable.tech/logging-credentials-secret")
+        {
+            let secret_api: Api<Secret> = Api::namespaced(client.clone(), &namespace);
+            if let Ok(credentials) = secret_api.get(credentials_secret).await {
+                if let Some(username) = credentials
+                    .data
+                    .as_ref()
+                    .and_then(|data| data.get("username"))
+                    .and_then(|value| String::from_utf8(value.0.to_owned()).ok())
+                {
+                    let mut credentials_info = format!("Username: {username}");
+                    if redact_credentials {
+                        credentials_info.push_str(&format!(", password: {REDACTED_PASSWORD}"));
+                    } else if let Some(password) = credentials
+                        .data
+                        .as_ref()
+                        .and_then(|data| data.get("password"))
+                        .and_then(|value| String::from_utf8(value.0.to_owned()).ok())
+                    {
+                        credentials_info.push_str(&format!(", password: {password}"));
+                    }
+                    extra_infos.push(credentials_info);
+                }
+            }
+        }
+
+        let installed_product = InstalledProduct {
+            name: service.name_unchecked(),
+            namespace: service.namespace(),
+            endpoints,
+            extra_infos,
+        };
         result.push(installed_product);
     }
 
     Ok(result)
-}
-
-pub async fn get_opensearch_dashboards_service(
-    name: &str,
-    namespace: &str,
-    client: Client,
-    redact_credentials: bool,
-) -> Result<InstalledProduct, Box<dyn Error>> {
-    let service_api: Api<Service> = Api::namespaced(client.clone(), namespace);
-
-    let service = service_api.get(name).await?;
-    let mut endpoints = get_service_endpoint_urls(&service, name, client.clone()).await?;
-
-    if let Some(http_endpoint) = endpoints.get("http") {
-        endpoints.insert(
-            "logs".into(),
-            format!("{http_endpoint}/app/discover?security_tenant=global#/view/logs"),
-        );
-    }
-
-    let extra_infos = vec![
-        "Third party service".to_string(),
-        format!(
-            "Admin user: {username}, password: {password}",
-            username = "admin",
-            password = if redact_credentials {
-                REDACTED_PASSWORD
-            } else {
-                "admin"
-            }
-        ),
-    ];
-
-    Ok(InstalledProduct {
-        name: name.to_string(),
-        namespace: Some(namespace.to_string()),
-        endpoints,
-        extra_infos,
-    })
 }
